a060cfd9018d508d8069301b3fb3ce1e7f453062
priyank8141 <jainpriyank789@gmail.com>
2020-12-22 13:19:16 +0530

    new module farmer

---
+++ farmer/urls.py
@@ -0,0 +1,11 @@
+
+from django.contrib import admin
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+
+    # path('login', views.show_login, name="login"),
+    # path('logprocess', views.logproess,name='logproess')
+]
--- login/views.py
+++ login/views.py
@@ -1,14 +1,15 @@
 import psycopg2
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from signup.models import user
 # Create your views here.
 def show_login(request):
     print("this is login page")
     return render(request, "login.html")
-conn = psycopg2.connect(database="agriculture", user='postgres', password='priyank8141', host='127.0.0.1', port='5432')
-cursor = conn.cursor()
 
 def logproess(request):
+    conn = psycopg2.connect(database="agriculture", user='postgres', password='priyank8141', host='127.0.0.1',port='5432')
+    cursor = conn.cursor()
+
     print("this is login process")
     if (request.method == 'POST'):
         try:
@@ -18,7 +19,19 @@ def logproess(request):
             cursor.execute(Query, (email,))
             records = cursor.fetchall()
             print(records)
-            print(records[0][0])
+            # print(records[0][0])
+            if records[0][10] == password:
+                print("success login")
+                response = redirect('/redirectfarmer/')
+                return response
+                # return render(request, "successlogin.html")
+
+            else:
+                context = {
+                'message': 'invalid password and username'
+                }
+                return render(request, "login.html", context)
+
             # for row in records:
             # print("Id = ", row[0], )
             #     print("Model = ", row[1])
@@ -32,7 +45,7 @@ def logproess(request):
             #     return render(request, "signup.html")
             #     print("success login")
         except (Exception, psycopg2.Error) as error:
-            print("Failed to insert record error:-", error)
+            print("error:-", error)
 
         finally:
         # closing database connection.
